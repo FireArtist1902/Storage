0d52f8f297c8d7bd711a9fd87229d3a0b663a855
FireArtist1902 <115476894+FireArtist1902@users.noreply.github.com>
2024-02-05 19:29:16 +0200

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,32 @@
+﻿CREATE DATABASE Storage
+
+Use Storage
+
+Create table Product
+(
+	[Id] INT primary key,
+	[Name] nvarchar(50) not null,
+	[TypeId] int not null,
+	[Count] int not null,
+	[CostPrice] money not null,
+	[DateOfDelivery] date not null
+);
+
+Create table ProdType
+(
+	[Id] int primary key,
+	[Name] nvarchar(50) not null
+);
+
+Create table ProductToProvider
+(
+	[Id] int primary key,
+	[ProviderId] int not null,
+	[ProductId] int not null
+);
+
+Create table ProdProvider
+(
+	[Id] int primary key,
+	[Name] nvarchar(50) not null
+);
